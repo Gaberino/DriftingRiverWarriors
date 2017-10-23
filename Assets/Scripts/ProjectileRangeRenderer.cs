47aae2f0fabac52e5c44ecaa97a9daf2e8b61237
Unknown <gr1157@nyu.edu>
2017-10-23 18:31:06 -0400

    Fixed more stuff

--- Assets/Scripts/ProjectileRangeRenderer.cs
+++ Assets/Scripts/ProjectileRangeRenderer.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 //this goes on the arrows themselves
 public class ProjectileRangeRenderer : MonoBehaviour {
 
-	public float renderMultiplier = 10f;
 
 	private Projectile myProj;
 	private LineRenderer myLR;
@@ -19,7 +18,7 @@ public class ProjectileRangeRenderer : MonoBehaviour {
 	void Update () {
 		if (!myProj.fired){
 			myLR.SetPosition(0, this.transform.position + this.transform.up * this.transform.localScale.y / 2); //set point 1 to tip of arrow
-			myLR.SetPosition(1, (this.transform.position + this.transform.up * this.transform.localScale.y / 2) + (Vector3)myProj.velocity * myProj.life * renderMultiplier); //this ends where the arrow will fall
+			myLR.SetPosition(1, (this.transform.position + this.transform.up * this.transform.localScale.y / 2) + (Vector3)myProj.velocity * myProj.life * 60); //this ends where the arrow will fall
 		}
 		else {
 			Debug.Log("Ley line seal");
